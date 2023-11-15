035db5ab3d989f47951c5cf8f1351c862061acf1
Oh011 <145376235+Oh011@users.noreply.github.com>
2023-11-15 16:52:42 +0200

    Update application.cpp

--- application.cpp
+++ application.cpp
@@ -1,1 +1,37 @@
 
+
+//File: application.cpp
+
+/* purpose: a Vole machine simulator that can execute it's instructions and run the program and
+  it gives the user a menu to interact with the machine  providing options to load a program from a file and run
+  it while displaying the machine */
+
+//Author 1:omar Hisham abdallah, 20220235
+//Author 2:Fady Ashraf Eskandr ,20221245
+//Author 3:Eslam Eid Mohamed Othman,20210066
+//section: s17,s18
+
+// Date: 14 Nov 2023
+
+#include <bits/stdc++.h>
+#include "vole.h"
+
+using namespace std;
+
+
+int main() {
+
+   machine vole;
+
+/*This function serves as a menu for the user to interact with the machine,
+ providing options to load a program from a file and run it while displaying the machine
+ state.*/
+
+   vole.menu();
+
+
+
+
+
+    return 0;
+}
