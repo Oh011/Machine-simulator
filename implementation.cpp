96019ba003d796674e2767c6cdbc28966ca3f87d
fady ashraf <125466577+fadyashraf7@users.noreply.github.com>
2023-11-15 17:58:24 +0200

    Update implementation.cpp

--- implementation.cpp
+++ implementation.cpp
@@ -3,3 +3,45 @@
 
 using namespace std;
 
+
+
+memory::memory() {
+
+    int count1 = 48;
+    int count2 = 48;
+    string temp;
+
+    // Loop over 16 rows in memory
+    for (int i = 0; i <16; ++i) {
+
+        temp.push_back(char(count1));
+
+        // Loop over 16 columns in each row
+        for (int j = 0; j <16; ++j) {
+
+            temp.push_back(char(count2));
+
+            // Initialize each cell in memory with "00"
+            cells[temp] = "00";
+
+            // Update the second character in the cell's address
+            if (char(count2) == '9')
+                count2 += 8;
+            else
+                ++count2;
+
+            temp.pop_back(); // Remove the last character for the next iteration
+
+        }
+
+        // Update the first character in the cell's address
+        if (char(count1) == '9')
+            count1 += 8;
+        else
+            ++count1;
+
+        count2 = 48; // Reset the second character
+        temp = "";   // Reset the temporary string for the next row
+    }
+}
+
