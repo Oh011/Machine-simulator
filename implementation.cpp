d296749af85ece18425ddcd86e08b63c9d17975d
fady ashraf <125466577+fadyashraf7@users.noreply.github.com>
2023-11-15 18:15:33 +0200

    Update implementation.cpp
    add function from 545 to 609

--- implementation.cpp
+++ implementation.cpp
@@ -114,3 +114,69 @@ string decimal_to_binary(int x) {
     // This makes the binary representation from right to left.
     return decimal_to_binary(x / 2) + to_string(x % 2);
 }
+
+void cpu::ADD(char t, string s1, string s2) {
+
+    // Variables to store the two hexadecimal digits in the form of binary strings.
+    string temp1_s1;
+    string temp2_s1;
+    string temp1_s2;
+    string temp2_s2;
+
+    // this Function converts hexa digits of the registers content
+    // after converting every hexa digit to decimal then to binary
+    // and perform  transformation on two binary strings.
+    fun(s1, s2, temp1_s1, temp2_s1, temp1_s2, temp2_s2);
+
+    // add the two  digits after converting each digit from hexa to binary.
+    // to obtain complete binary numbers.
+    string num1 = temp2_s1.append(temp1_s1);
+    string num2 = temp2_s2.append(temp1_s2);
+
+
+
+    // Variables for binary addition.
+    int rem = 0; // Remainder
+    int sum = 0; // Sum of corresponding bits
+    string add; // Result of addition
+
+    // Perform binary addition from right to left.
+
+    for (int i = 8; i >= 0; --i) {
+        sum = (int(num1[i]) - 48) + (int(num2[i]) - 48) + rem;
+
+        // Update result and remainder based on the sum.
+        if (sum == 0) {
+            add.push_back('0');
+            rem = 0;
+        } else if (sum == 1) {
+            add.push_back('1');
+            rem = 0;
+        } else if (sum == 2) {
+            add.push_back('0');
+            rem = 1;
+        } else if (sum == 3) {
+            add.push_back('1');
+            rem = 1;
+        }
+    }
+
+    // Reverse the result to obtain the correct binary addition.
+    std::reverse(add.begin(), add.end());
+
+    // Check for overflow conditions.
+    if (add[0] == '1' && (num1[0] == '0' && num2[0] == '0'))
+        cout << "overflow has occurred\n";
+    else if (add[0] == '0' && (num1[0] == '1' && num2[0] == '1'))
+        cout << "overflow has occurred\n";
+
+    // Store the result in the specified register after converting it to hexadecimal.
+    string n = "R";
+    n.push_back(t);
+    reg[n].content = binary_to_hexa(add);
+
+    return;
+}
+
+
+// Performs bitwise OR operation on two binary strings and stores the result
