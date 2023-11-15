845f55cb9d3501931f21eea4d2af7dc3412691ed
eslameidmohamed <102488678+eslameidmohamed@users.noreply.github.com>
2023-11-15 18:33:16 +0200

    Update implementation.cpp

--- implementation.cpp
+++ implementation.cpp
@@ -529,3 +529,62 @@ void cpu::XOR(char t, string s1, string s2) {
     // Store the result in the specified register after converting it to hexadecimal.
     reg[n].content = binary_to_hexa(temp);
 }
+
+
+int char_to_int(char x) {
+    // Use a switch statement to map each hexadecimal character to its corresponding integer value.
+    switch (x) {
+        case '0':
+            return 0;
+
+        case '1':
+            return 1;
+
+        case '2':
+            return 2;
+
+        case '3':
+            return 3;
+
+        case '4':
+            return 4;
+
+        case '5':
+            return 5;
+
+        case '6':
+            return 6;
+
+        case '7':
+            return 7;
+
+        case '8':
+            return 8;
+
+        case '9':
+            return 9;
+
+        case 'A':
+            return 10;
+
+        case 'B':
+            return 11;
+
+        case 'C':
+            return 12;
+
+        case 'D':
+            return 13;
+
+        case 'E':
+            return 14;
+
+        case 'F':
+            return 15;
+
+            // Handle the case where the input is not a valid hexadecimal character.
+        default:
+            return -1; // Return -1 to indicate an error or an invalid input.
+    }
+}
+
